62040518fb54605d73f199faa2e63cc5b8ea18e6
Collin Gress <gress126@gmail.com>
2017-07-26 22:46:10 -0500

    adds simple script to parse out our pseudo-json output files and write them to mongo

---
+++ benchmark/benchmark_runner/json_to_db.py
@@ -0,0 +1,20 @@
+import json
+import sys
+
+from db_writer import DBWriter
+
+try:
+    output_json = open(sys.argv[1])
+except IndexError:
+    print "Must provide location of json file. python json_to_db.py <PATH_TO_JSON>"
+    exit()
+
+with open('run_config.json') as rc:
+    run_config = json.load(rc)
+
+writer = DBWriter(run_config)
+
+with open(sys.argv[1]) as output_json:
+    for line in output_json:
+        writer.write(json.loads(line))
+

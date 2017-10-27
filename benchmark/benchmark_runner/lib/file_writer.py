d50bff8afe3f2d4030d01d8ac6c3f4f588ea5ff8
Collin Gress <gress126@gmail.com>
2017-10-27 09:50:10 -0500

    bug fix

--- benchmark/benchmark_runner/lib/file_writer.py
+++ benchmark/benchmark_runner/lib/file_writer.py
@@ -7,9 +7,10 @@ class FileWriter:
     def __init__(self, run_config):
         self.file_target = run_config["file_target"]
     def write(self, entry, filename = None):
-        filename = datetime.now().strftime('%Y-%m-%d-%H-%M-%S-%f') 
-        filename += json.dumps(entry)
-        filename = hashlib.md5(filename).hexdigest()
+        if not filename:
+            filename = datetime.now().strftime('%Y-%m-%d-%H-%M-%S-%f') 
+            filename += json.dumps(entry)
+            filename = hashlib.md5(filename).hexdigest()
         with open(self.file_target + "/" + filename + ".out", "a") as f:
             json.dump(entry, f) 
             f.write('\n')

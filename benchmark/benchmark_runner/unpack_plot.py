94602fbd7bf2192c3d371e6c8db5544ae53f7d5e
Collin Gress <gress126@gmail.com>
2017-11-06 11:19:14 -0600

    plotting is good

--- benchmark/benchmark_runner/unpack_plot.py
+++ benchmark/benchmark_runner/unpack_plot.py
@@ -28,28 +28,30 @@ def json_str_to_title(json_s):
 def no_unicode_keys(data):
     return dict((str(key), value) for (key, value) in data.items())
 
-def plot_entry(data, title = "", x = "iterations", y = "timing", line_base = "container_size"):
+def plot(data, title = "", x = "iterations", y = "timing", line_base = "container_size"):
     plt.figure(figsize = (19.2, 10.8), dpi = 100)
     plt.grid(linestyle = 'dashed')
     line_base_dict = dict()
     for entry in data:
         entry = no_unicode_keys(entry)
-        l_base = entry[line_base]
-        if l_base in line_base_dict:
-            line_base_dict[l_base].append(entry)
+        entry['timing'] = np.mean(entry['timing'])
+        l_base_val = entry[line_base] # creating a separate line based on this value
+        if l_base_val in line_base_dict:
+            line_base_dict[l_base_val].append(entry) # add a set of timings into this line base dict
         else:
-            line_base_dict[l_base] = [entry]
-    for base in line_base_dict:
+            line_base_dict[l_base_val] = [entry] 
+    for base in line_base_dict: # actually plot the line now
         data_ = { 'x': [], 'y': [] }
         for element in line_base_dict[base]:
             data_['x'].append(float(element[x]))
-            data_['y'].append(np.mean(element[y]))
+            data_['y'].append(float(element[y]))
         data_frame = df.from_dict(data_)
         data_frame.sort('x', inplace=True)
         ax = plt.gca()
-        print(data_frame)
         ax.plot(data_frame['x'], data_frame['y'], label = line_base + " = " + str(base))
         ax.set_xscale('log')
+        ax.set_xlabel(x)
+        ax.set_ylabel(y)
         plt.title(title)
         plt.legend()
     filename = title.replace("\n", "").replace(",", "_").replace(" = ", "-")
@@ -64,13 +66,21 @@ def plot_entry(data, title = "", x = "iterations", y = "timing", line_base = "co
     plt.close()
     
 reader = DBReader(run_config)
-variable = ["container_size", "iterations", "timing"]
-constant = ["access_pattern", "compiler", "complexity", "container", "optimization", \
-        "orientation", "type", "column"]
+
+dim_set = ["container_size", "iterations", "timing", "access_pattern", "compiler", "complexity", \
+        "container", "optimization", "orientation", "type", "column"]
+line_basis = "orientation"
+x_axis = "iterations"
+y_axis = "timing"
+variable = [x_axis, y_axis, line_basis]
+constant = list(set(dim_set) - set(variable))
 
 query_set, vdict = reader.createQueriesFromCartesian(variable, constant)
+qset_len = len(query_set)
+progress = 0
 for query in query_set:
+    progress += 1
     title = json_str_to_title(json.dumps(query))
-    print(title)
+    print('(' + str(progress) + '/' + str(qset_len) + ') ' + title)
     data = reader.query(query, vdict)
-    plot_entry(data, title, "iterations", "timing", "container_size")
+    plot(data, title, x_axis, y_axis, line_basis)
